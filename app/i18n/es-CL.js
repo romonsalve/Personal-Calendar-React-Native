21721c96f4b3f85525b035d91eb00a3af3117496
Rodrigo Monsalve Lagos <rodrigo@MacBook-Air-de-Rodrigo.local>
2019-10-29 13:05:24 -0300

    Pseudo Localization

---
+++ app/i18n/es-CL.js
@@ -0,0 +1,23 @@
+const es_CL = {
+  booking: {
+    label: 'Cita',
+    actions: {
+      cancel: 'Cancelar cita',
+    },
+    cancelAlert: {
+      title: ({ service }) => `Cancelar la cita ${service}`,
+      message: 'No podrás acceder a la cita nuevamente y deberás contactar a soporte para recuperarla',
+    },
+  },
+  commons: {
+    edit: 'Editar',
+    back: 'Volver',
+  },
+  screens: {
+    bookingDetail: {
+      headerTitle: 'Detalles de la cita',
+    },
+  },
+};
+
+export default es_CL;
