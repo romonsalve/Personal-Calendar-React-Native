8ec0881c7998d49fdac2b8478606af162207ebcc
Rodrigo Monsalve Lagos <rodrigo@MacBook-Air-de-Rodrigo.local>
2019-10-28 22:12:34 -0300

    Correct Styles, Add Header edit button

--- app/views/BookingDetailScreen.js
+++ app/views/BookingDetailScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Buttons } from '../styles';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Buttons, Spacing } from '../styles';
 import moment from 'moment';
 
 const styles = StyleSheet.create({
@@ -10,19 +10,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   bookingInfo: {
-    
+    flex: 1,
   },
-  time: {
+  timeContainer: {
     flex: 1,
     flexDirection: 'row',
   },
+  timeText: {
+    marginRight: Spacing.extraSmall,
+  },
   clientInfo: {
-
+    flex: 1,
   },
   cancelButton: {
-
-  }
+  },
 });
+
+function renderEditButton() {
+  return (
+    <Button
+      onPress={() => alert('This is a button!')}
+      title='Editar'
+    />
+  );
+}
 
 export default function BookingDetailScreen({ navigation }) {
 
@@ -34,16 +45,18 @@ export default function BookingDetailScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.bookingInfo}>
+        
         <Text>{booking.service}</Text>
         <Text>{price}</Text>
         <Text>{moment(booking.start).format(formatDate) }</Text>
-        <View style={styles.time}>
-          <Text>{moment(booking.start).format(formatTime) }</Text>
-          <Text>{moment(booking.end).format(formatTime) }</Text>
+        <View style={styles.timeContainer}>
+          <Text style={styles.timeText}>{moment(booking.start).format(formatTime) }</Text>
+          <Text style={styles.timeText}>{moment(booking.end).format(formatTime) }</Text>
         </View>
       </View>
       
       <View style={styles.clientInfo}>
+        <Text>Cliente</Text>
         <Text>{clientName}</Text>
         <Text>{booking.client.email}</Text>
       </View>
@@ -51,3 +64,8 @@ export default function BookingDetailScreen({ navigation }) {
     </View>
   );
 }
+
+BookingDetailScreen.navigationOptions = {
+  headerTitle: 'Detalles de la cita',
+  headerRight: renderEditButton,
+};
